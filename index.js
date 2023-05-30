44f85b0d165683acc3b294538879aeb240d87ca4
tomas-aguiler <vamos2020tomi@gmail.com>
2023-05-30 20:20:12 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const miBoton = document.getElementById("miBoton");
+function click() {
+    alert("¡Hola! Has hecho click en el boton");
+}
+miBoton.addEventListener("click", function(click){
+    alert("¡haz hecho click en el boton");
+})
---
+++ script.js
@@ -0,0 +1,6 @@
+function showAlert() { alert("hola, esto es una alerta de javascript");}
+
+function showAlert() { alert("HOLAA"); }
+let boton = document.getElementById("miboton");
+boton.addEventListener("click",function()
+{alert("haz hecho click");});
